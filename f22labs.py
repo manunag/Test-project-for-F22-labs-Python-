3c130ef3908bb9ba0f9d532c7664f6056593b0ce
manunag <36444982+manunag@users.noreply.github.com>
2018-08-09 17:24:49 +0530

    Update f22labs.py

--- f22labs.py
+++ f22labs.py
@@ -1,3 +1,4 @@
+
 alpha=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 y=0
 a=[]
@@ -18,13 +19,20 @@ for i in range(6):
     z[y]=0
 print(x)
 inp=[]
+guess=20
 inp1=inp
 same=0
 diff=0
-while same!=6:
+print("Welcome to the F22Labs Game")
+print("There is a 6 letter word selected by me,Try to Guess the word (Capital letters only)")
+while same!=6 and guess>0:
     same=0
     diff=0
+
     inp=input();
+    while(len(inp)>6 or len(inp)<6):
+        print("Enter the Word with length of 6 characters")
+        inp=input()
     for i in range(6):
         if x[i]==inp[i]:
             same=same+1
@@ -32,5 +40,17 @@ while same!=6:
             for j in range(6):
                 if x[i]==inp[j]:
                     diff=diff+1
-    print("diff",diff)
-    print("same",same)
+
+    print("\nNumber of characters that are correct, but in the wrong place are ",diff)
+    print("Number of characters that are correct and in the correct place are",same)
+
+    if same==6:
+        print("Congratulations you found the string")
+        break;
+    guess=guess-1
+    if guess==0:
+        print("Game Over")
+        break;
+    print("Guesses remaining:",guess)
+    print("Please try again\n")
+    print("Guess the word")
